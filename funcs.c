7ada6ec7ffb40fec17d36c24bdd213d9d778379e
Ed Harcourt <edharcourt@gmail.com>
2020-01-27 20:49:17 -0500

    data_plan_calc.c

---
+++ funcs.c
@@ -0,0 +1,12 @@
+
+// declare the function before you define it
+extern void f();
+
+// main returns an int
+int main() {
+    f();
+
+    return 0;  // retern zero if no error otherwise non-zero
+}
+
+void f() { }
---
+++ numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Messing around with number systems
+
+void print_binary(int d) {
+    // finish on wednesday
+}
+
+int main() {
+    int bits = 0b1011101;
+    printf("%d\n", bits);
+
+    bits = 0b11101101;
+    printf("%d\n", bits);
+}
