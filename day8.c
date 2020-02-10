604955b882a2f3e3bb0102040dfeb3bc06cdb2ed
Ed Harcourt <edharcourt@gmail.com>
2020-02-10 18:19:28 -0500

    day8.c

---
+++ day8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    short int x = 0xFFFF;   // -1
+
+    int y = 0xFFFF;  // 2^16 - 1
+
+    int z = 0xFFFFFFFF;
+
+    int a = 1000000000;
+    int b = 2000000000;
+    int c = a + b;
+    printf("%d\n", c);
+
+    // 67 - 23
+    int d = 67 + (~23 + 1);
+    printf("%d\n", d);
+
+    // print the location of variable d
+    // & - address of
+    printf("%x\n", &d);
+
+    return 0;
+}
