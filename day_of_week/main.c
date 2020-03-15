cf5a69555f4eb5639491bb7d19a6ae29888c9ea1
Ed Harcourt <edharcourt@gmail.com>
2020-03-15 10:28:48 -0400

    .

--- day_of_week/main.c
+++ day_of_week/main.c
@@ -1,9 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dow.h"
 
-int weekday(int month, int day, int year) {
-    
-    int y0 = year - (14 - month) / 12;
-    int x = y0 + y0/4 - y0/100 + y0/400;  // leap year
-    int m0 = month + 12  * ((14 - month) / 12) - 2;
-    int d0 = (day + x + (31 * m0) / 12) % 7;
-    return d0;
+/*
+ *  What day was Feb 14, 2000 on?
+ *
+ *  ./day 2 14 2000
+ *  Monday
+ *
+ */
+int main(int argc, char **argv) {
+    int month = atoi(argv[1]);
+    int day = atoi(argv[2]);
+    int year = atoi(argv[3]);
+
+    int dow = weekday(month, day, year);
+    printf("DEBUG: %d\n", dow);
+
+    char *days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday",
+                     "Thursday", "Friday", "Saturday"};
+
+    //printf("%s\n", days[dow]);
 }
+
+
